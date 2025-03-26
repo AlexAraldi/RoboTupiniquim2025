e0e15da7f80149d2535094763118504cfc6b97b5
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-26 17:12:47 -0300

    Implementado o Limite do Grid de atuação do robo.

--- RoboTupiniquim2025/Program.cs
+++ RoboTupiniquim2025/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        // Criar Grid Limite de atuação do Robo
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Robo Tupiniquim - Missão Marte");
+
+            Console.Write("Digite a coordenada A do mapa: ");
+            int coordenadaA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite a coordenada B do mapa: ");
+            int coordenadaB = Convert.ToInt32(Console.ReadLine());
+                        
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
