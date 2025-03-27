9d82f2c8abe2789c34ffc2490ba6843b04f93255
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-27 16:50:59 -0300

    Implementado a logica do movimento D, E e M.

--- RoboTupiniquim2025/Program.cs
+++ RoboTupiniquim2025/Program.cs
@@ -1,49 +1,96 @@
-﻿namespace RoboTupiniquim2025
+﻿using System.IO;
+using System.Runtime.Intrinsics.X86;
+
+namespace RoboTupiniquim2025
 {
+    // A área escolhida para análise é curiosamente retangular e os robôs devem andar por ela.
+    // A posição de um robô é representada por uma combinação de coordenadas X e Y e também uma letra representando a direção que ele está olhando.
+    // Use as orientações: N = norte, S = sul, L = leste, O = oeste.
+    // E, D e M. As letras E e D fazem o robô virar 90 graus para esquerda e direita respectivamente,sem sair do lugar.
+    // A letra M significa se mover uma posição no grid para frente, mantendo a mesma direção.
+    // Assuma que mover o robô para frente, significa mover sua posição de (X, Y) para (X, Y+1).
+    // Cada comando deve ser enviado em 2 partes: a primeira parte é a posição inicial do robô
+    // e a segunda uma série de instruções que aquele robô deve seguir para explorar a área.
+    // A posição é dada com 2 inteiros e uma letra, separados por espaços,correspondendo pelas coordenadas X e Y e a orientação do robô.
+
     internal class Program
     {
-        //-------------------------------------------------Criar Grid Limite de atuação do Robo
         static void Main(string[] args)
         {
-            Console.WriteLine("---------------------------------------------");
-            Console.WriteLine("Robo Tupiniquim - Missão Marte");
-            Console.WriteLine("---------------------------------------------");
-
-            Console.Write("Digite a coordenada X do mapa: ");
-            int limiteX = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Digite a coordenada Y do mapa: ");
-            int limiteY = Convert.ToInt32(Console.ReadLine());
-
-            Console.ReadLine();
-
-            //------------------------------------------------Criar entrada da movimentação do robô
-
-
-            Console.Write("Digite a coordenada A do Robô: ");
-            int coordenadaA = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Digite a coordenada B do Robô: ");
-            int coordenadaB = Convert.ToInt32(Console.ReadLine());
-
-            Console.ReadLine();
-
-            //-------------------------------------------------Criar a lógica limite de atuação
-
-            for (int posicaoX = 0; posicaoX < limiteX; posicaoX++)
+            while (true)
             {
-                if (posicaoX > 0)
+                Console.Clear();
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Robo Tupiniquim - Missão Marte");
+                Console.WriteLine("---------------------------------------------");
+
+                //------------------------------------------------Criar entrada da posição inicial do robô do grid
+
+                string comando = "EMEMEMEMM";
+
+                char[] instrucoes = comando.ToCharArray();
+
+                int mapaGridX = 5;
+                int mapaGridY = 5;
+                char direcao = 'N';
+                int posicaoX = 1;
+                int posicaoY = 2;
+
+                for (int i = 0; i < instrucoes.Length; i++)
                 {
+                    if (instrucoes[i] == 'D')
+                    {
+                        if (direcao == 'N')
+                            direcao = 'L';
+                        else if (direcao == 'L')
+                            direcao = 'S';
+                        else if (direcao == 'S')
+                            direcao = 'O';
+                        else if (direcao == 'O')
+                            direcao = 'N';
+                    }
+
+                    if (instrucoes[i] == 'E')
+                    {
+                        if (direcao == 'N')
+                            direcao = 'O';
+                        else if (direcao == 'L')
+                            direcao = 'N';
+                        else if (direcao == 'S')
+                            direcao = 'L';
+                        else if (direcao == 'O')
+                            direcao = 'S';
+
+                    }
+                    if (instrucoes[i] == 'M')
+                    {
+                        if (direcao == 'N')
+                            posicaoY++;
+                        else if (direcao == 'S')
+                            posicaoY--;
+                        else if (direcao == 'O')
+                            posicaoX--;
+                        else if (direcao == 'L')
+                            posicaoX++;
+                    }
+
+
+
+
+
 
 
                 }
-
-
-
-
+                    else if (instrucoes[i] == 'M')
+                {
+                    posicaoY++;
+                }
 
 
             }
+            Console.ReadLine();
         }
     }
 }
+}
+
